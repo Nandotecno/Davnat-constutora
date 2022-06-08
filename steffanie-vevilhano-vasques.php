7a4f081ba8359b9d309ef71e41f2156c70e2167a
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2022-06-08 20:38:38 -0300

    Add files via upload

---
+++ steffanie-vevilhano-vasques.php
@@ -0,0 +1,22 @@
+<div class="cab" style="background: url('../assets/img/headers/sobre-bg.jpg') no-repeat;">
+    <div class="text-center">
+        <h2>Nuestro equipo</h2>
+    </div>
+</div>
+
+<section class="inner-page">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h2>Steffanie Sevilhano Vasques</h2>
+                <hr>                
+                <ul>
+                    <li>Graduada en Derecho por la Universidad Católica de Santos. (2014-2018)</li>
+                    <li>Inglés e Español</li>
+            </div>
+            <div class="col-md-4">
+                <img src="../assets/img/team/DraSteffanie.jpg" alt="Steffanie Sevilhano Vasques" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
